2fbdc1c57a5af542571cba68aa390947fbf80fbf
Amo020816 <1461713136@qq.com>
2022-09-25 13:10:13 +0800

    Vector

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include "ArrayList/ArrayList.h"
 #include "ArrayList/ArrayList.cpp"
-
-ArrayList<int> list1(20);
+#include "Vector/Vector.h"
+#include "Vector/Vector.cpp"
 
 using namespace std;
+
+Vector<int> vec1;
+
 int main()
 {
+    vec1.PushBack(10);
+    cout << *vec1.Back() << endl;
 
-    for (int i = 0; i < 10; i++)
-        list1.insert(i, i);
-    list1.insert(5, 11);
-    list1.clean();
-    list1.insert(0, 1100);
-    for (int i = 0; i < list1.size(); i++)
-        cout << list1.get(i) << ' ';
-    cout << endl;
+    Vector<int> vec2(10);
+    vec2.PushBack(100);
+    vec2.PushBack(1000);
+
+    vec1 = std::move(vec2);
+    cout << vec1[0] << ' ' << vec1[1] << endl;
 
     return 0;
 }
